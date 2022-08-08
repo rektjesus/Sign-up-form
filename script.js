72c66f54d37055433811d56bf089c0b5e8dad06c
sleepless <arnas.manucan@gmail.com>
2022-08-08 15:10:43 +0300

    Add proper validation to input fields

--- script.js
+++ script.js
@@ -3,15 +3,30 @@ const signUp = document.querySelector('.sign_up');
 const section = document.querySelector('section');
 const toggleBtn = document.querySelectorAll('a');
 
+const newPwd = document.querySelector('#new_pwd');
+const repeatPwd = document.querySelector('#repeat_pwd');
+
+function comparePwd(newPwd, repeatPwd) {
+    if (newPwd.value === repeatPwd.value) {
+        repeatPwd.setCustomValidity('')
+    }
+    else if (newPwd.value !== repeatPwd.value) {
+        repeatPwd.setCustomValidity('Passwords do not match!');
+    }
+};
+
 function toggleForm() {
     signIn.classList.toggle('active');
     signUp.classList.toggle('active');
     section.classList.toggle('active');
-}
+};
 
 toggleBtn.forEach(button => {
     button.addEventListener('click', () => {
         toggleForm();
     })
-})
+});
 
+repeatPwd.addEventListener('keyup', () => {
+    comparePwd(newPwd, repeatPwd);
+});
